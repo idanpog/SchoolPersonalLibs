3241427951672effa466fc56bd41828e226da161
idanpog <40134558+idanpog@users.noreply.github.com>
2018-06-10 10:26:44 +0300

    i did a BUILD

--- IdansShortcuts/UserInput.cs
+++ IdansShortcuts/UserInput.cs
@@ -10,6 +10,7 @@ namespace IdansShortcuts
     {
         /// <summary>
         /// Nuggets!!!!!!!!!!!!!!!!!!!!!!
+        /// it's a Test function.
         /// </summary>
         public static void nuggets()
         {
@@ -56,6 +57,10 @@ namespace IdansShortcuts
             else if (option == 20)
             { Console.WriteLine("Im disappointed, I could have never imagined that I'll end my life begging to a piece of bones and flash for " + add); }
         }
+
+        /// <summary>
+        /// gets an int input from the user.
+        /// </summary>
         public static int Int(string add)
         {
             int output, option = 0, counter = 1;
@@ -78,6 +83,9 @@ namespace IdansShortcuts
             }
             return output;
         }
+        /// <summary>
+        /// gets a double input from the user.
+        /// </summary>
         public static double Double(string add)
         {
             int option = 0, counter = 1;
